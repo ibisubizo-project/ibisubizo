3bb1e9a559655d30f9f947b6508e10998f3491e6
Ofonime Usoro <ofonimeusoro01@gmail.com>
2019-05-05 18:16:00 +0200

    Load More Button functionality implemented

--- src/containers/ListingContainer.js
+++ src/containers/ListingContainer.js
@@ -1,45 +1,64 @@
 import React from 'react'
 import Listing from './Listing';
 import { loadProblems, loadUsersProblem } from '../actions/problems';
-import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import UserProfile from '../utils/userProfile'
+import problemsApi from '../services/problemsApi'
 
 
 class ListingContainer extends React.Component{
-    state = {
-        showListings: true,
-        showPersonalListing: false,
-        isAuthenticated: false,
-        problems: [],
-        userProblems: [],
-        page: 1,
+    constructor(props) {
+        super(props)
+        this.state = {
+            loading: false,
+            showListings: true,
+            showPersonalListing: false,
+            isAuthenticated: false,
+            problems: [],
+            userProblems: [],
+            page: 1,
+            hasMore: true, 
+            error: ''
+        }
+        this.getAllProblems = this.getAllProblems.bind(this)
+        this.getAllUsersProblems = this.getAllUsersProblems.bind(this)
+        this.loadMore = this.loadMore.bind(this)
+        this.getUserProblems = this.getUserProblems.bind(this)
     }
 
-    fetchFirstProblems() {
+
+    //Fetching all problems whether the user is authenticated or not 
+    getAllProblems() {
         let { page } = this.state
+        this.setState({loading: true})
+        problemsApi.getAllProblems(page).then(result => {
+            console.dir(result)
+            result.length === 0 ? this.setState({hasMore: false}) : this.setState({problems: [...this.state.problems, ...result], loading: false})
+        }).catch(error => this.setState({error: error, loading: false}))
+    }
+
+    componentDidMount() {
+        let { page } = this.state
+        this.getAllProblems(page) //Get the first initial problems
         let userData = JSON.parse(localStorage.getItem("userData"))
         if(userData !== null) {
             if(this.props.data.isAuthenticated === true && userData._id !== null ) {
                 console.log("result[1]")
-                this.props.fetchUsersProblem(userData._id, page)
-                this.setState({isAuthenticated: true, showPersonalListing: true, problems: this.props.problems, userProblems: this.props.userProblems})
+                this.getAllUsersProblems(userData._id)
             }
-        } else {
-            console.log("result[2]")
-            this.props.onFetchProblems(page)
-            this.setState({showPersonalListing: false, showListings: true})
         }
-    }
-
-    componentDidMount() {
-        this.fetchFirstProblems()
     }
 
     getUserProblems(event) {
         event.preventDefault()
-        this.setState({showPersonalListing: true, showListings: false, problems: [...this.props.problems, ...this.state.problems] , userProblems: this.props.userProblems})
+        this.setState({showPersonalListing: true, showListings: false})
+    }
+
+    getAllUsersProblems(user_id) {
+        problemsApi.getAllUsersProblems(user_id, 0).then(result => {
+            console.dir(result)
+            this.setState({isAuthenticated: true, userProblems: result})
+        })
     }
 
     showLatest(event) {
@@ -50,9 +69,8 @@ class ListingContainer extends React.Component{
     loadMore() {
         this.setState(prevState => ({
             page: prevState.page + 1
-        }), this.fetchFirstProblem)
+        }), this.getAllProblems)
     }
-
     render() {
         return (
             <div className="listings mt-6">
@@ -62,12 +80,12 @@ class ListingContainer extends React.Component{
 
                         <div className="flex justify-between">
                             <div className="mr-3 cursor-pointer" onClick={this.showLatest.bind(this)}>Latest Problems</div>
-                            <div className="cursor-pointer" onClick={this.getUserProblems.bind(this)}>Mine</div  >
+                            <div className="cursor-pointer" onClick={this.getUserProblems.bind(this)}>Mine</div>
                         </div>
                     </div>
                 </div>
-                {this.state.showPersonalListing && !this.state.showListings ?  (<Listing problems={this.props.userProblems} />) : (<Listing problems={this.props.problems} />)}
-                <div onClick={this.loadMore.bind(this)} className="mx-auto container">Load More</div>
+                {this.state.showPersonalListing && !this.state.showListings ?  (<Listing problems={this.state.userProblems} />) : (<Listing problems={this.state.problems} />)}
+                {!this.state.showPersonalListing & this.state.hasMore ? (<div onClick={this.loadMore}>Load More</div>): ''}
             </div>
         )
     }
