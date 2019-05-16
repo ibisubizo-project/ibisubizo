fa9a4b6503f30fd1e697e78d424f399338db8bf8
Ofonime Usoro <ofonimeusoro01@gmail.com>
2019-05-16 03:48:18 +0300

    Added NavBar for the production build

--- src/containers/Root.prod.js
+++ src/containers/Root.prod.js
@@ -3,18 +3,27 @@ import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
 import { Router, Route, Switch } from 'react-router-dom'
 import App from './App'
+import NavBar from '../components/Nav';
 import Login from '../components/Login'
 import Register from '../components/Register'
 import history from '../../src/history'
+import DetailComponent from './DetailComponent'
+import AboutUsComponent from '../components/AboutUsComponent';
+import TermsAndConditionComponent from '../components/TermsAndConditionComponent';
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router history={history}>
       <div>
+        <NavBar />
         <Switch>
           <Route exact path='/' component={App} />
+          <Route path='/bye' component={()=> <h1>Bye Bye</h1>} />
           <Route path='/auth/login' component={Login} />
           <Route path='/auth/register' component={Register} />
+          <Route path='/problem/:id' component={DetailComponent} />
+          <Route path='/about' component={AboutUsComponent} />
+          <Route path='/term' component={TermsAndConditionComponent} />
           <Route path={'*'} component={()=> <h1>Page Not Found...</h1>} />
         </Switch>
       </div>
@@ -22,11 +31,8 @@ const Root = ({ store }) => (
   </Provider>
 )
 
-
 Root.propTypes = {
     store: PropTypes.object.isRequired,
 }
-
-
 
 export default Root
