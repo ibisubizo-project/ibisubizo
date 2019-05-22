0b6a194968b9ec2c41990a8a4ea52199ed752ef5
Ofonime Usoro <ofonimeusoro01@gmail.com>
2019-05-22 16:24:39 +0200

    Fixed minor bug with nav auth links display

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,37 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom'
 
+import {connect} from 'react-redux'
 
 
-const NavBar = () => {
+
+class NavBar extends Component {
+  render() {
     return (
-        <div className="container mx-auto bg-teal h-12 text-white pl-10">
-            <div className="flex justify-between">
-                <ul className="list-reset inline-block flex mt-3">
-                    <li className="mr-2">
-                        <Link className="text-white no-underline" to="/">Home</Link>
-                    </li>
-                    <li className="mr-2">
-                        <Link className="text-white no-underline" to="/about">About Us</Link>
-                    </li>
-                    <li className="mr-2">
-                        <Link className="text-white no-underline" to="/term">Terms & Conditions</Link>
-                    </li>
-                </ul>
+      <div className="container mx-auto bg-teal h-12 text-white pl-10">
+        <div className="flex justify-between">
+          <ul className="list-reset inline-block flex mt-3">
+            <li className="mr-2">
+              <Link className="text-white no-underline" to="/">Home</Link>
+            </li>
+            <li className="mr-2">
+              <Link className="text-white no-underline" to="/about">About Us</Link>
+            </li>
+            <li className="mr-2">
+              <Link className="text-white no-underline" to="/term">Terms & Conditions</Link>
+            </li>
+          </ul>
 
-                <div className="navbar-right">
-                    <ul className="list-reset inline-block flex mt-3">
-                        <li className="mr-2">
-                            <Link className="text-white no-underline" to="/auth/login">Login</Link>
-                        </li>
-                        <li className="mr-2">
-                            <Link className="text-white no-underline" to="/auth/register">Register</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+          {!this.props.userIsAuthenticated &&
+            (
+              <div className="navbar-right">
+                <ul className="list-reset inline-block flex mt-3">
+                  <li className="mr-2">
+                    <Link className="text-white no-underline" to="/auth/login">Login</Link>
+                  </li>
+                  <li className="mr-2">
+                    <Link className="text-white no-underline" to="/auth/register">Register</Link>
+                  </li>
+                </ul>
+              </div>
+            )
+          }
         </div>
+      </div>
     )
+  }
+    
 }
 
-export default NavBar;
+const mapStateToProps = (state) => {
+    return {
+        userIsAuthenticated: state.usersReducer.isAuthenticated
+    }
+}
+
+
+export default connect(mapStateToProps, null)(NavBar)
