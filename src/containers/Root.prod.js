bae4987fb3f340feeb65cc5b2c278056f371d4b7
Ofonime Usoro <ofonimeusoro01@gmail.com>
2019-05-30 17:51:07 +0200

    Minor change

--- src/containers/Root.prod.js
+++ src/containers/Root.prod.js
@@ -11,6 +11,7 @@ import history from '../../src/history'
 import DetailComponent from './DetailComponent'
 import AboutUsComponent from '../components/AboutUsComponent';
 import TermsAndConditionComponent from '../components/TermsAndConditionComponent';
+import PageNotFound from '../components/PageNotFound';
 
 const Root = ({ store }) => (
   <Provider store={store}>
@@ -25,7 +26,7 @@ const Root = ({ store }) => (
           <PrivateRoute path='/problem/:id' component={DetailComponent} />
           <Route path='/about' component={AboutUsComponent} />
           <Route path='/term' component={TermsAndConditionComponent} />
-          <Route path={'*'} component={()=> <h1>Page Not Found...</h1>} />
+          <Route path={'*'} component={PageNotFound} />
         </Switch>
       </React.Fragment>
     </Router>
