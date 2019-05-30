b1031eed0b5605e5c869c2d25da42fe6ff2db07d
Ofonime Usoro <ofonimeusoro01@gmail.com>
2019-05-30 11:28:48 +0200

    Refactoring static pages

--- src/components/AboutUsComponent.js
+++ src/components/AboutUsComponent.js
@@ -3,9 +3,9 @@ import React from 'react'
 
 const AboutUsComponent = () => {
     return (
-        <div className="container m-auto p-8 text-grey-darkest">
+        <div className="container m-auto h-screen p-8 text-grey-darkest">
             <div className="w-3/5 px-6 py-4">
-                <h1>About Us</h1>
+                <h1 className="text-3xl">About Us</h1>
                 <p className="mt-5 p-0">Ibisubizo.com is an online platform owned by Thousand Skills Ltd, a company which uses sciences and technology to design and provide solutions needed by its clients. </p>
                 <p className="mt-5">Ibisubizo.com allows users to submit their individual and/or social issues so that they can be fixed by professionals</p>
             </div>
--- src/components/TermsAndConditionComponent.js
+++ src/components/TermsAndConditionComponent.js
@@ -6,18 +6,18 @@ const TermsAndConditionComponent = () => {
         <div className="container m-auto p-8 text-grey-darkest">
             <div className="w-3/5 px-6 py-4">
                 <div className="term-condition mt-10">
-                    <h1>TERMS AND CONDITIONS </h1>
+                    <h1 className="text-3xl mb-3">TERMS AND CONDITIONS </h1>
                     <p>These terms and conditions outline the rules and regulations for the use of Ibisubizo.com's Website.  </p>
                     <p className="mt-2 mb-2">By accessing this website we assume you accept these terms and conditions in full. Do not continue to use Ibisubizo.com website if you do not accept all of the terms and conditions stated on this page.</p>
                     <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and any or all Agreements: "Client", "You" and "Your" refers to you, the person accessing this website and accepting the Company's terms and conditions. "The Company", “Thousand Skills Ltd”, "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves, or either the Client or ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner, whether by formal meetings of a fixed duration, or any other means, for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services/products, in accordance with and subject to, prevailing law of. Any use of the above terminology or other words in the singular, plural, capitalisation and/or he/she or they, are taken as interchangeable and therefore as referring to same. </p>
 
                     <div className="cookies mt-3">
-                        <h4>Cookies</h4>
+                        <h4 className="text-2xl">Cookies</h4>
                         <p>We employ the use of cookies. By using Ibisubizo.com's website you consent to the use of cookies in accordance with Ibisubizo.com's privacy policy. Most of the modern day interactive web sites use cookies to enable us to retrieve user details for each visit. Cookies are used in some areas of our site to enable the functionality of this area and ease of use for those people visiting. Some of our affiliate / advertising partners may also use cookies.</p>
                     </div>
 
                     <div className="license mt-3">
-                        <h4>License</h4>
+                        <h4 className="text-2xl">License</h4>
                         <p>Unless otherwise stated, Ibisubizo.com and/or it's licensors own the intellectual property rights for all material on Ibisubizo.com. All intellectual property rights are reserved. You may view and/or print pages from http://ibisubizo.com for your own personal use subject to restrictions set in these terms and conditions.</p>
 
                         <p>You must not:</p>
@@ -30,7 +30,7 @@ const TermsAndConditionComponent = () => {
                     </div>
 
                     <div className="user-comment mt-3">
-                        <h4>User Comments</h4>
+                        <h4 className="text-2xl">User Comments</h4>
                         This Agreement shall begin on the date hereof.
                         Certain parts of this website offer the opportunity for users to post and exchange opinions, information, material and data ('Comments') in areas of the website. Comments do not reflect the views or opinions of Ibisubizo.com, its agents or affiliates. Comments reflect the view and opinion of the person who posts such view or opinion. To the extent permitted by applicable laws Ibisubizo.com shall not be responsible or liable for the Comments or for any loss cost, liability, damages or expenses caused and or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.
                         Ibisubizo.com reserves the right to monitor all Comments and to remove any Comments which it considers in its absolute discretion to be inappropriate, offensive or otherwise in breach of these Terms and Conditions.
@@ -43,7 +43,7 @@ const TermsAndConditionComponent = () => {
                     </div>
 
                     <div className="hyperlinking mt-3">
-                        <h4>Hyperlinking to our Content</h4>
+                        <h4 className="text-2xl">Hyperlinking to our Content</h4>
                         The following organizations may link to our Web site without prior written approval:
                         <ul>
                             <li>Government agencies;</li>
@@ -57,30 +57,30 @@ const TermsAndConditionComponent = () => {
                     </div>
 
                     <div className="iframes mt-4">
-                        <h4>Iframes</h4>
+                        <h4 className="text-2xl">Iframes</h4>
                         <p>Without prior approval and express written permission, you may not create frames around our Web pages or use other techniques that alter in any way the visual presentation or appearance of our Web site.</p>
                     </div>
 
                     <div className="reservation_rights mt-4">
-                        <h4>Reservation of Rights</h4>
+                        <h4 className="text-2xl">Reservation of Rights</h4>
                         <p>We reserve the right at any time and in its sole discretion to request that you remove all links or any particular link to our Web site. You agree to immediately remove all links to our Web site upon such request. We also reserve the right to amend these terms and conditions and its linking policy at any time. By continuing to link to our Web site, you agree to be bound to and abide by these linking terms and conditions.</p>
                     </div>
 
                     <div className="link_removal mt-4">
-                        <h4>Removal of links from our website</h4>
+                        <h4 className="text-2xl">Removal of links from our website</h4>
                         <p>If you find any link on our Web site or any linked web site objectionable for any reason, you may contact us about this. We will consider requests to remove links but will have no obligation to do so or to respond directly to you.
                         Whilst we endeavour to ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we commit to ensuring that the website remains available or that the material on the website is kept up to date.</p>
                     </div>
 
                     <div className="content_liability mt-4">
-                        <h4>Content Liability</h4>
+                        <h4 className="text-2xl">Content Liability</h4>
                         <p>
                             We shall have no responsibility or liability for any content appearing on your Web site. You agree to indemnify and defend us against all claims arising out of or based upon your Website. No link(s) may appear on any page on your Web site or within any context containing content or materials that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.
                         </p>
                     </div>
                     
                     <div className="disclaimer mt-4">
-                        <h4>Disclaimer</h4>
+                        <h4 className="text-2xl">Disclaimer</h4>
                         <p>
                         To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website (including, without limitation, any warranties implied by law in respect of satisfactory quality, fitness for purpose and/or the use of reasonable care and skill). Nothing in this disclaimer will:
                         limit or exclude our or your liability for death or personal injury resulting from negligence;
@@ -93,7 +93,7 @@ const TermsAndConditionComponent = () => {
 
                 <div className="privacy_policy mt-5">
                     <div>
-                        <h2>PRIVACY POLICY</h2>
+                        <h2 className="text-2xl">PRIVACY POLICY</h2>
                         <p>Your privacy is critically important to us.</p>
                         <p><a href="Ibisubizo.com">Ibisubizo.com</a></p>
                         <p>It is Ibisubizo.com's policy to respect your privacy regarding any information we may collect while operating our website. This Privacy Policy applies to ibisubizo.com (hereinafter, "us", "we", or "ibisubizo.com"). We respect your privacy and are committed to protecting personally identifiable information you may provide us through the Website. We have adopted this privacy policy ("Privacy Policy") to explain what information may be collected on our Website, how we use this information, and under what circumstances we may disclose the information to third parties. This Privacy Policy applies only to information we collect through the Website and does not apply to our collection of information from other sources.
@@ -101,43 +101,43 @@ const TermsAndConditionComponent = () => {
                     </div>
 
                     <div className="visitors mt-5">
-                        <h4>Website Visitors</h4>
+                        <h4 className="text-2xl">Website Visitors</h4>
                         <p>Like most website operators, Ibisubizo.com collects non-personally-identifying information of the sort that web browsers and servers typically make available, such as the browser type, language preference, referring site, and the date and time of each visitor request. Ibisubizo.com's purpose in collecting non-personally identifying information is to better understand how Ibisubizo.com's visitors use its website. From time to time, Ibisubizo.com may release non-personally-identifying information in the aggregate, e.g., by publishing a report on trends in the usage of its website.
                         Ibisubizo.com also collects potentially personally-identifying information like Internet Protocol (IP) addresses for logged in users and for users leaving comments on http://ibisubizo.com. Ibisubizo.com only discloses logged in user and commenter IP addresses under the same circumstances that it uses and discloses personally-identifying information as described below.</p>
                     </div>
 
                     <div className="pii mt-4">
-                        <h4>Gathering of Personally-Identifying Information</h4>
+                        <h4 className="text-2xl">Gathering of Personally-Identifying Information</h4>
                         <p>Certain visitors to Ibisubizo.com's websites choose to interact with Ibisubizo.com in ways that require Ibisubizo.com to gather personally-identifying information. The amount and type of information that Ibisubizo.com gathers depends on the nature of the interaction. For example, we ask visitors who sign up at http://ibisubizo.com to provide a username and email address.</p>
 
                     </div>
 
                     <div className="security mt-4">
-                        <h4>Security</h4>
+                        <h4 className="text-2xl">Security</h4>
                         <p>The security of your Personal Information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security.</p>
                     </div>
 
                     <div className="advertisement mt-4">
-                        <h4>Advertisements</h4>
+                        <h4 className="text-2xl">Advertisements</h4>
                         <p>Ads appearing on our website may be delivered to users by advertising partners, who may set cookies. These cookies allow the ad server to recognize your computer each time they send you an online advertisement to compile information about you or others who use your computer. This information allows ad networks to, among other things, deliver targeted advertisements that they believe will be of most interest to you. This Privacy Policy covers the use of cookies by Ibisubizo.com and does not cover the use of cookies by any advertisers.</p>
                     </div>
 
                     <div className="external_sites mt-4">
-                        <h4>Links To External Sites</h4>
+                        <h4 className="text-2xl">Links To External Sites</h4>
                         <p>Our Service may contain links to external sites that are not operated by us. If you click on a third party link, you will be directed to that third party's site. We strongly advise you to review the Privacy Policy and terms and conditions of every site you visit.</p>
                         <p>We have no control over, and assume no responsibility for the content, privacy policies or practices of any third party sites, products or services.</p>
 
                     </div>
 
                     <div className="protection mt-4">
-                        <h4>Protection of Certain Personally-Identifying Information</h4>
+                        <h4 className="text-2xl">Protection of Certain Personally-Identifying Information</h4>
                         <p>Ibisubizo.com discloses potentially personally-identifying and personally-identifying information only to those of its employees, contractors and affiliated organizations that (i) need to know that information in order to process it on Ibisubizo.com's behalf or to provide services available at Ibisubizo.com's website, and (ii) that have agreed not to disclose it to others. Some of those employees, contractors and affiliated organizations may be located outside of your home country; by using Ibisubizo.com's website, you consent to the transfer of such information to them. Ibisubizo.com will not rent or sell potentially personally-identifying and personally-identifying information to anyone. Other than to its employees, contractors and affiliated organizations, as described above, Ibisubizo.com discloses potentially personally-identifying and personally-identifying information only in response to a subpoena, court order or other governmental request, or when Ibisubizo.com believes in good faith that disclosure is reasonably necessary to protect the property or rights of Ibisubizo.com, third parties or the public at large.</p>
 
                         <p>If you are a registered user of http://ibisubizo.com and have supplied your email address, Ibisubizo.com may occasionally send you an email to tell you about new features, solicit your feedback, or just keep you up to date with what's going on with Ibisubizo.com and our products. Ibisubizo.com takes all measures reasonably necessary to protect against the unauthorized access, use, alteration or destruction of potentially personally-identifying and personally-identifying information.</p>
                     </div>
 
                     <div className="aggregated_statistics mt-4">
-                        <h4>Aggregated Statistics</h4>
+                        <h4 className="text-2xl">Aggregated Statistics</h4>
                         <p>Ibisubizo.com may collect statistics about the behavior of visitors to its website. Ibisubizo.com may display this information publicly or provide it to others. However, Ibisubizo.com does not disclose your personally-identifying information.</p>
                     </div>
 
@@ -147,7 +147,7 @@ const TermsAndConditionComponent = () => {
                     </div>
 
                     <div className="cookies mt-4">
-                        <h4>Cookies</h4>
+                        <h4 className="text-2xl">Cookies</h4>
                         <p>To enrich and perfect your online experience, Ibisubizo.com uses "Cookies", similar technologies and services provided by others to display personalized content, appropriate advertising and store your preferences on your computer.</p>
                         <p>A cookie is a string of information that a website stores on a visitor's computer, and that the visitor's browser provides to the website each time the visitor returns. Ibisubizo.com uses cookies to help Ibisubizo.com identify and track visitors, their usage of http://ibisubizo.com, and their website access preferences. Ibisubizo.com visitors who do not wish to have cookies placed on their computers should set their browsers to refuse cookies before using Ibisubizo.com's websites, with the drawback that certain features of Ibisubizo.com's websites may not function properly without the aid of cookies.</p>
                         <p>By continuing to navigate our website without changing your cookie settings, you hereby acknowledge and agree to Ibisubizo.com's use of cookies.</p>
@@ -155,16 +155,15 @@ const TermsAndConditionComponent = () => {
                     </div>
 
                     <div className="mt-4">
-                        <h4>Business Transfers</h4>
+                        <h4 className="text-2xl">Business Transfers</h4>
                         <p>If Ibisubizo.com, or substantially all of its assets, were acquired, or in the unlikely event that Ibisubizo.com goes out of business or enters bankruptcy, user information would be one of the assets that is transferred or acquired by a third party. You acknowledge that such transfers may occur, and that any acquirer of Ibisubizo.com may continue to use your personal information as set forth in this policy.</p>
                     </div>
 
                     <div className="privacy_policy mt-4 mb-10">
-                        <h4>Privacy Policy Changes</h4>
+                        <h4 className="text-2xl">Privacy Policy Changes</h4>
                         <p>Although most changes are likely to be minor, Ibisubizo.com may change its Privacy Policy from time to time, and in Ibisubizo.com's sole discretion. Ibisubizo.com encourages visitors to frequently check this page for any changes to its Privacy Policy. Your continued use of this site after any change in this Privacy Policy will constitute your acceptance of such change.	</p>
                         <p>For your request of information about our terms and conditions and Privacy Policy, you can write to us at thousandskillsltd@gmail.com  </p>
                     </div>
-
                 </div>
             </div>
         </div>
