6cb69bc94bb9ed8cff9c05de9d8e79f48d9e9e36
Ofonime Usoro <ofonimeusoro01@gmail.com>
2019-06-16 08:09:31 +0200

    Trending problems implemented

--- src/services/metricsApi.js
+++ src/services/metricsApi.js
@@ -8,6 +8,7 @@ const metricsApi = {};
 metricsApi.getProblemMetrics = (problemId) => client.get(`/metrics/${problemId}`)
 metricsApi.addProblemMetrics = (metrics) => client.post(`/metrics/`, metrics)
 metricsApi.updateProblemMetrics = (problemId, metrics) => client.put(`/metrics/${problemId}`, metrics)
+metricsApi.getTrendingProblems = () => client.get("/metrics/trendings")
 
 
 export default metricsApi;
