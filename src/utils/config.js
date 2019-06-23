cf33660490289250517516d48656195cbd87c192
Ofonime Usoro <ofonimeusoro01@gmail.com>
2019-06-23 22:22:12 +0200

    Changing API URL

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@
 
 const Config = {
     APP_URL : process.env.APP_URL || 'https://ibisubizo.com',
-    API_URL: process.env.API_URL || 'http://api.ibisubizo.com/api'
+    API_URL: process.env.API_URL || 'https://api.ibisubizo.com/api'
     //API_URL: 'http://localhost:8000/api'
 }
 
