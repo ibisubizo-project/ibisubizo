cfdbf9e43a65e418cdedf1e354edbed73cda1e20
Ofonime Usoro <ofonimeusoro01@gmail.com>
2019-06-27 18:03:18 +0200

    Updated URL

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,8 +2,8 @@
 
 const Config = {
     APP_URL : process.env.APP_URL || 'https://ibisubizo.com',
-    //API_URL: process.env.API_URL || 'https://api.ibisubizo.com/api'
-    API_URL: 'http://localhost:8000/api'
+    API_URL: process.env.API_URL || 'https://app.ibisubizo.com/api'
+    //API_URL: 'http://localhost:8000/api'
 }
 
 export default Config;
